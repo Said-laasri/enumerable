0df6697d9ad45f0ee8bf7e54f437b730a998c072
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-22 13:20:08 +0100

    Add any? methode to module

--- class.rb
+++ class.rb
@@ -10,4 +10,12 @@ module MyEnumerable
     end
     puts arr.length == list.length
   end
+
+  def any?
+    arr = []
+    list.each do |i|
+      arr.push(i) if yield(i)
+    end
+    puts !arr.empty?
+  end
 end
