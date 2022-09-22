2de5ef97d0386e90dbe9a8c2c5e2ceab3d076935
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-22 13:21:17 +0100

    Add filter? methode to module

--- class.rb
+++ class.rb
@@ -18,4 +18,12 @@ module MyEnumerable
     end
     puts !arr.empty?
   end
+
+  def filter?
+    arr = []
+    list.each do |i|
+      arr.push(i) if yield(i)
+    end
+    puts arr.to_s
+  end
 end
